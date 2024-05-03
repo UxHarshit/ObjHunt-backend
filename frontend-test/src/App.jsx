376afedcb9a67bc54ab9d1163df2e55af59b2461
Kaushik <akaushikarkar@gmail.com>
2024-05-03 13:42:51 +0530

    added a demo frontend for testing purposes

---
+++ frontend-test/src/App.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useMemo, useState } from 'react'
+import './App.css'
+import { connect, io } from "socket.io-client";
+
+function App() {
+  const [message, setMessage] = useState([]);
+  const [user, setUser] = useState("");
+  const socket = useMemo(() => io("http://localhost:6969"), [])
+
+  function handleClick(e) {
+    e.preventDefault();
+    socket.emit("createRoom", { username: user })
+    setUser("");
+  }
+
+  useEffect(() => {
+    socket.on('newplayer', (e) => {
+      console.log(e)
+      setMessage(prevMessage => [...prevMessage, e])
+    })
+    socket.on('message', (e) => {
+      console.log(e)
+      setMessage(prevMessage => [...prevMessage, e])
+    })
+
+
+    return () => {
+      socket.disconnect()
+    }
+  }, [])
+
+  function handleChange(e) {
+    e.preventDefault();
+    setUser(e.target.value);
+    console.log(user)
+  }
+
+  function handleRandom(e) {
+    e.preventDefault();
+    socket.emit("joinRandom", { username: user })
+  }
+
+  return (
+    <>
+      <input type="text" name="" placeholder='username' onChange={handleChange} value={user} id="" />
+      <button onClick={handleRandom}>Join Random</button>
+      <button onClick={handleClick}>Create Room</button>
+      <div>{message.map((e) => <h6 id='e'>{e}</h6>)}</div>
+    </>
+  )
+}
+
+export default App
