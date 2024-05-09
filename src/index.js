59f72b72c8a2c6825e741c18f985ea7f051ab7e6
Kaushik <akaushikarkar@gmail.com>
2024-05-09 21:43:15 +0530

    add comments to index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
+//Importing packgages
 import express from "express";
 import http from "http";
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
+
+//Importing functions
 import RoomRouter from "./routes/room.js";
 import { initializeSocket } from "./socket/socket.js";
-import dotenv from 'dotenv';
-dotenv.config({path: "./.env"});
 
+//Setting up server
 const app = express();
 const httpServer = http.createServer(app);
 const port = process.env.PORT || 6969;
 
+//Routes
 app.use("/room", RoomRouter);
 
+//Starting server
 httpServer.listen(port, () => console.log(`server listening on port ${port}`));
 
-initializeSocket(httpServer);
+initializeSocket(httpServer); //Starting socket.io
