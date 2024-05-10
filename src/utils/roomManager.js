5396e16e8d4949945a076be475ae8c2318b48314
Kaushik <akaushikarkar@gmail.com>
2024-05-10 17:20:11 +0530

    refactor some code and add comments to roomManager.js

--- src/utils/roomManager.js
+++ src/utils/roomManager.js
@@ -4,6 +4,7 @@ dotenv.config({ path: "./.env" });
 const rooms = [];
 const OBJECTS = ["bottle", "remote", "bat", "object4"];
 
+//Filling up rooms array with empty rooms. (looking for a better approach).
 for (let i = 0; i < process.env.MAX_ROOMS; i++) {
   rooms.push({
     id: i,
@@ -14,8 +15,9 @@ for (let i = 0; i < process.env.MAX_ROOMS; i++) {
   });
 }
 
+//Function to return an empty room id
 function assignNewRoom() {
-  let availableRooms = rooms.filter((val, ind) => val.players.length === 0);
+  let availableRooms = rooms.filter((room) => room.players.length === 0);
   if (availableRooms.length === 0) {
     return undefined;
   } else {
@@ -23,6 +25,7 @@ function assignNewRoom() {
   }
 }
 
+//Function to return a room id (doesn't need to be empty, but can't be full)
 function assignRoom() {
   let availableRooms = rooms.filter(
     (room) => room.players.length < process.env.MAX_PLAYERS
@@ -34,7 +37,9 @@ function assignRoom() {
   }
 }
 
+//Function to add a user to a room
 function addPlayer(user, socketId, roomId) {
+  //Finding the room
   for (let room = 0; room < rooms.length; room++) {
     if (rooms[room].id == roomId) {
       // Loop to check if player is duplicate
@@ -56,7 +61,9 @@ function addPlayer(user, socketId, roomId) {
   }
 }
 
+//Function to remove player from a room
 function removePlayer(socketId, roomId) {
+  //Loop to find the room
   for (let room = 0; room < rooms.length; room++) {
     if (rooms[room].id == roomId) {
       const index = rooms[room].players.findIndex(
@@ -69,6 +76,8 @@ function removePlayer(socketId, roomId) {
     }
   }
 }
+
+//Function to generate leaderboard
 function generateLeaderboard(roomId) {
   const room = rooms.find((room) => room.id === roomId);
   if (!room) {
@@ -83,23 +92,27 @@ function generateLeaderboard(roomId) {
   // Sort leaderboard by points in descending order
   leaderboard.sort((a, b) => b.points - a.points);
 
-  // Send the leaderboard data to the server
-  sendLeaderboardToServer(roomId, leaderboard);
-
   return leaderboard;
 }
 
-function sendLeaderboardToServer(roomId, leaderboard) {
-  // Simulate sending data to the server
-}
-// Example usage of modifyPoints function
-
+//Function to get a room's details
 function GetRoomDetails(roomId) {
   const room = rooms.find((room) => room.id === roomId);
   if (!room) return 0;
   return room;
 }
 
+/*
+ * Function to assign points to a user
+ *
+ * expects to get these as arguments:
+ *** room's id.
+ *** user's id.
+ *** an array of players who has already submitted (and their image is correct).
+ *** if the image is correct or not.
+ *
+ * This function assigns (process.env.MAX_PLAYERS) points to user if he's the first one to submit else decrease 1 point for every user that has already submitted (and their image is correct)
+ */
 const assignPoints = (roomId, userId, playersArray, isCorrectObject) => {
   const room = rooms.findIndex((room) => room.id === roomId);
   if (room === -1) {
@@ -122,6 +135,7 @@ const assignPoints = (roomId, userId, playersArray, isCorrectObject) => {
   return 1;
 };
 
+//Function to set user's hasSubmitted flag to true
 function updateSubmitted(userId, roomId) {
   const room = rooms.findIndex((room) => room.id === roomId);
   if (room === -1) {
@@ -136,35 +150,45 @@ function updateSubmitted(userId, roomId) {
   rooms[room].players[playerInd].hasSubmitted = true;
 }
 
+// Function to set room's playing status to the provided value (true || false)
 function setPlaying(roomId, val) {
-  rooms.forEach((room, ind) => {
+  for (let ind = 0; ind < rooms.length; ind++) {
+    const room = rooms[ind];
     if (room.id === roomId) {
       rooms[ind].isPlaying = val;
       console.log(rooms[ind]);
+      break;
     }
-  });
+  }
 }
 
+//Function to set the round number of a room
 function setRound(roomId, round) {
-  rooms.forEach((room, ind) => {
+  for (let ind = 0; ind < rooms.length; ind++) {
+    const room = rooms[ind];
     if (room.id === roomId) {
       rooms[ind].round = round;
       console.log(rooms[ind]);
+      break;
     }
-  });
+  }
 }
 
+//Function to reset stats (hasSubmitted, isCorrect) of all players in a room
 function resetStats(roomId) {
-  rooms.forEach((room, ind) => {
+  for (let ind = 0; ind < rooms.length; ind++) {
+    const room = rooms[ind];
     if (room.id === roomId) {
-      rooms[ind].players.forEach((player, playerInd) => {
+      for (let playerInd = 0; playerInd < room.players.length; playerInd++) {
         rooms[ind].players[playerInd].hasSubmitted = false;
         rooms[ind].players[playerInd].isCorrect = false;
-      });
+      }
+      break;
     }
-  });
+  }
 }
 
+//Function to select a random object and also update room's current_obj property
 function generateObject(roomId) {
   const roomInd = rooms.findIndex((room) => room.id == roomId);
   if (roomInd === -1) {
