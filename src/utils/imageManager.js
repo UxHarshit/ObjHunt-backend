3794f6c24deb457544873199364ec19ad6dd446d
uxharshit <harshitkatheria7890@gmail.com>
2024-05-20 18:49:03 +0530

    Obj detection

--- src/utils/imageManager.js
+++ src/utils/imageManager.js
@@ -1,4 +1,7 @@
 import fs from "fs";
+import * as cocoSsd from "@tensorflow-models/coco-ssd";
+import * as tf from "@tensorflow/tfjs-node";
+
 
 const checkImage = (filename, buffer, object) => {
   //Saving image temporarily. images are not meant to save in production. the image will be sent to ML model to detect if the object matches the image.
@@ -7,6 +10,24 @@ const checkImage = (filename, buffer, object) => {
       console.error(err);
     } else {
       console.log("Image uploaded:", filename);
+      // Loading the model and image to check if the object is present in the image.
+      Promise.all([cocoSsd.load(), fs.promises.readFile("uploads/" + filename)])
+        .then((results) => {
+          const model = results[0];
+          // Decoding the image to tensor.
+          const img = tf.node.decodeImage(results[1], 3);
+          // Detecting the object in the image.
+          return model.detect(img);
+        }).then((predictions) => {
+          console.log("Predictions: ", predictions);
+          // Checking if the object is present in the image.
+          predictions.forEach((prediction) => {
+            if (prediction.class === object) {
+              return true
+            }
+          });
+          return false;
+        });
     }
   });
 
