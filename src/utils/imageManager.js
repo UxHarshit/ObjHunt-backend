a8aa6a9a1b0f89aa82c68f6827dab82adc3c4b38
uxharshit <harshitkatheria7890@gmail.com>
2024-05-31 22:02:10 +0530

    Fixed Accuracy using V2 MobileNet

--- src/utils/imageManager.js
+++ src/utils/imageManager.js
@@ -1,4 +1,5 @@
 import * as cocoSsd from "@tensorflow-models/coco-ssd";
+import * as mobilenet from "@tensorflow-models/mobilenet";
 import * as tf from "@tensorflow/tfjs-node";
 import fs from "fs/promises";
 
@@ -8,26 +9,34 @@ const checkImage = async (image, room) => {
     const name = Math.random();
     await fs.writeFile("uploads/" + name + ".jpg", image);
     console.log("Image uploaded");
+
+    // image is base64 encoded, so we need to decode it
+    const buffer = Buffer.from(image, "base64");
+
+    image = tf.node.decodeImage(buffer, 3); // 3 channels for RGB
     
-    // Loading the model and the image
-    const image = await fs.readFile("uploads/" + filename);
-    const [model, imageBuffer] = await Promise.all([
-      cocoSsd.load(),
-      image,
-    ]);
-
-    // Decoding the image to tensor
-    const img = tf.node.decodeImage(imageBuffer, 3);
-
-    // Detecting objects in the image
-    const predictions = await model.detect(img);
-    console.log("Predictions: ", predictions);
-
+    // Load the model
+    const model = await mobilenet.load(
+      {
+        version: 2, // mobilenet version 2 has better accuracy
+        /**
+         * alpha is a numeric value in [0, 1], which controls the width of the network.
+         * The value of alpha is multiplied to the number of filters in each layer.
+         * Higher alpha means more weights, which increases the capacity of the network.
+         * Lower alpha reduces the number of weights, which reduces the capacity of the network.
+         * ⚠ Higher alpha reduces the speed of the network (i.e., the time taken to make a prediction).
+         */
+        alpha: 1.0,
+      },
+      { strict: true } // called for unsupported operations must be handled strictly
+    );
     // Checking if the object is present in the image
-    for (let prediction of predictions) {
-      if (prediction.class === object) {
-        return true;
-      }
+    const predictions = await model.classify(image);
+    console.log(predictions);
+    const obj = predictions.find((prediction) => prediction.className === room);
+    if (obj) {
+      console.log("Object found");
+      return true;
     }
     return false;
   } catch (err) {
